3424fcac24b0268a9558b0062dbac0e5ede7f3b7
Herm4n <gromadzki@gromadzki.eu>
2015-05-05 00:18:09 +0200

    Dodanie pola tekstowego wieloliniowego oraz 'szukajki'

--- graf-Ramka1/src/MenuTest.java
+++ graf-Ramka1/src/MenuTest.java
@@ -1,48 +1,64 @@
 import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
 
 public class MenuTest extends JFrame implements ActionListener
 {
 	JMenuBar menuBar;
+	JButton btnSzukaj;
+	JTextField txfSzukany;
 	JMenu menuPlik, menuNarzedzia, menuPomoc, menuPodmenu, menuOpcje;
 	JMenuItem mOtworz, mZapisz, mZamknij, mNarz1, mNarz2, mNarz3, mOPrograme, mStronaWWW, mOpcja1, mOpcja2;
 	JCheckBoxMenuItem cbmOpcja1, cbmOpcja2;
+	JTextArea notatnik;
 	
 	public MenuTest()
 	{
 		setTitle("Demonstracja JMenuBar");
 		setSize(500, 500);
-		setBounds(100, 100, 400, 300);
+		setBounds(100, 100, 500, 650);
 		setLayout(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+//		******************************************************************************************
+//		********************************* IMPLEMENTACJA MENU *************************************
+//		******************************************************************************************
 		menuBar = new JMenuBar();
 		menuPlik = new JMenu("Plik");
 		menuNarzedzia = new JMenu("Narzedzia");
 		menuPomoc = new JMenu("Pomoc");
 		
 		setJMenuBar(menuBar);
-		
 //		MENU PLIK
 		menuBar.add(menuPlik);
 		
 		mOtworz = new JMenuItem("Otworz", 'O');
+		mOtworz.addActionListener(this);
 		menuPlik.add(mOtworz);
 		
 		mZapisz = new JMenuItem("Zapisz");
+		mZapisz.addActionListener(this);
 		menuPlik.add(mZapisz);
 		
 		menuPlik.addSeparator();
@@ -87,9 +103,36 @@ public class MenuTest extends JFrame implements ActionListener
 			menuPodmenu.add(mStronaWWW);
 			mStronaWWW.addActionListener(this);
 		menuPomoc.add(menuPodmenu);
+//		********************************* //IMPLEMENTACJA MENU ***********************************
 		
+		
+//		******************************************************************************************
+//		********************************* ELEMENTY OKNA ******************************************
+//		******************************************************************************************
+		
+//		********************************* Pole tekstowe wielolinijkowe ***************************
+		notatnik = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane(notatnik);
+		scrollPane.setBounds(0, 0, 500, 500);
+		add(scrollPane);
+		
+//		********************************* //Pole tekstowe wielolinijkowe *************************
+//		********************************* Szukajka ***********************************************
+		btnSzukaj = new JButton("Szukaj");
+		btnSzukaj.setBounds(50, 530, 150, 25);
+		btnSzukaj.addActionListener(this);
+		add(btnSzukaj);
+		txfSzukany = new JTextField();
+		txfSzukany.setBounds(220, 530, 150, 25);
+		add(txfSzukany);
+//		********************************* //Szukajka *********************************************
+		
+//		********************************* //ELEMENTY OKNA ****************************************
 	}
-// OBS£UGA AKCJI
+//	******************************************************************************************
+//	********************************* OBSLUGA AKCJI ******************************************
+//	******************************************************************************************
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
@@ -106,6 +149,79 @@ public class MenuTest extends JFrame implements ActionListener
 				JOptionPane.showMessageDialog(this, "Wiedzialem", " ", JOptionPane.WARNING_MESSAGE);
 			}
 		}
+//		******************************** obsluga szukajki ******************************************
+		else if (zrodlo == btnSzukaj)
+		{
+			String tekst = notatnik.getText();
+			String szukane = txfSzukany.getText();
+			String wystapienia = "";
+			int i = 0;
+			int index = 0;
+			int indexStart = 0;
+			while ((index = tekst.indexOf(szukane, indexStart)) != -1)
+			{
+				indexStart = index + szukane.length();
+				i++;
+				wystapienia = wystapienia + " " + index;
+			}
+			
+			JOptionPane.showMessageDialog(null, szukane + " wystapilo " + i + " razy: " + wystapienia);
+		}
+//		******************************** //obsluga szukajki ****************************************
+		
+//		******************************** OTWIERANIE PLIKU ******************************************
+		else if (zrodlo == mOtworz)
+		{
+			JFileChooser fc = new JFileChooser();
+			if  (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+			{
+				File plik = fc.getSelectedFile();
+				try
+				{
+					Scanner skaner = new Scanner(plik);
+					while (skaner.hasNextLine())
+					{
+						notatnik.append(skaner.nextLine() + "\n");
+					}
+					skaner.close();
+				} 
+				catch (FileNotFoundException e1)
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+
+//		******************************** //OTWIERANIE PLIKU ******************************************
+		
+//		********************************** ZAPISYWANIE PLIKU *****************************************
+		else if (zrodlo == mZapisz)
+		{
+			JFileChooser fc = new JFileChooser();
+			if (fc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION);
+			{
+				File plik = fc.getSelectedFile();
+				try
+				{
+					PrintWriter pw = new PrintWriter(plik);
+					Scanner skaner = new Scanner(notatnik.getText());
+					while (skaner.hasNextLine())
+					{
+						pw.println(skaner.nextLine() + "\n");
+											
+					}
+					pw.close();
+					skaner.close();
+				}
+				catch (Exception e2)
+				{
+					// TODO: handle exception
+				}
+			}
+		}
+//		********************************** //ZAPISYWANIE PLIKU ***************************************		
+//		****************************************************************************************
 		else if (zrodlo == cbmOpcja1)
 		{
 			if (cbmOpcja1.isSelected())
@@ -148,6 +264,7 @@ public class MenuTest extends JFrame implements ActionListener
 		}
 		
 	}
+//		********************************* //OBSLUGA AKCJI ******************************************
 
 	public static void main(String[] args)
 	{
