1768c8adeba9ba882fdff42bc1c4d328cbf2e7de
Herm4n <gromadzki@gromadzki.eu>
2015-05-06 22:02:17 +0200

    Dodanie testow na JSlider

---
+++ graf-Ramka1/src/JSliderTest.java
@@ -0,0 +1,79 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+public class JSliderTest extends JFrame implements ChangeListener
+{
+	private JLabel lblCel, lblFah;
+	private JSlider sldCel, sldFah;
+	private int tempCel, tempFah;
+	
+	public JSliderTest()
+	{
+		setSize(700,300);
+		setTitle("Test JSlider'a");
+		setLayout(null);
+		
+		sldCel = new JSlider(0, 100, 0);
+		sldCel.setBounds(20, 50, 500, 50);
+		sldCel.setMajorTickSpacing(10);
+		sldCel.setMinorTickSpacing(5);
+		sldCel.setPaintTicks(true);
+		sldCel.setPaintLabels(true);
+		sldCel.addChangeListener(this);
+		add(sldCel);
+		
+		sldFah = new JSlider(30, 212, 30);
+		sldFah.setBounds(20, 150, 500, 50);
+		sldFah.setMajorTickSpacing(20);
+		sldFah.setMinorTickSpacing(16);
+		sldFah.setPaintTicks(true);
+		sldFah.setPaintLabels(true);
+		sldFah.addChangeListener(this);
+		sldFah.setEnabled(false);
+		add(sldFah);
+		
+		lblCel = new JLabel("Celsius: 0");
+		lblCel.setBounds(550, 50, 250, 25);
+		add(lblCel);
+		
+		lblFah = new JLabel("Fahrenheit: 0");
+		lblFah.setBounds(550, 150, 250, 25);
+		add(lblFah);
+	}
+	public static void main(String[] args)
+	{
+		JSliderTest app = new JSliderTest();
+		app.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		app.setVisible(true);
+
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e)
+	{
+		Object z = e.getSource();
+		if (z == sldCel)
+		{
+			tempCel = sldCel.getValue();
+			lblCel.setText("Celsius: " + tempCel);
+			tempFah = (int)Math.round(32 + (9.0/5.0)*tempCel);
+			sldFah.setValue(tempFah);
+			lblFah.setText("Fahrenheit: " + tempFah);
+		}
+		else if (z == sldFah)
+		{
+			tempFah = sldFah.getValue();
+			lblFah.setText("Fahrenheit: " + tempFah);
+			tempCel = (int)Math.round((tempFah - 32.0) * (5.0 / 9.0));
+			sldCel.setValue(tempCel);
+			lblCel.setText("Celsius: " + tempCel);
+		}
+	}
+
+}
--- graf-Ramka1/src/PasswordFieldTest.java
+++ graf-Ramka1/src/PasswordFieldTest.java
@@ -1,15 +1,28 @@
+import java.awt.ScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.NotActiveException;
 import java.util.Arrays;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.text.StyledEditorKit.BoldAction;
 
 
 public class PasswordFieldTest extends JFrame implements ActionListener
 {
-	JPasswordField psfHaslo;
+	private PanelHasla PanelHasla;
+	private JTextArea txaNotatnik;
+	private JScrollPane scrollPane;
+	private JButton btnDodajUzytkownika;
 	
 	public PasswordFieldTest()
 	{
@@ -17,12 +30,16 @@ public class PasswordFieldTest extends JFrame implements ActionListener
 		setSize(400 , 400);
 		setLayout(null);
 		
-		psfHaslo = new JPasswordField();
-		psfHaslo.setBounds(50, 50, 150, 25);
-		psfHaslo.setEchoChar('x');
-		psfHaslo.addActionListener(this);
+		txaNotatnik = new JTextArea();
+		txaNotatnik.setWrapStyleWord(true);
+		scrollPane = new JScrollPane(txaNotatnik);
+		scrollPane.setBounds(0, 0, 300, 300);
+		add(scrollPane);
 		
-		add(psfHaslo);
+		btnDodajUzytkownika = new JButton("Dodaj uzytkownika");
+		btnDodajUzytkownika.setBounds(50, 330, 160, 25);
+		btnDodajUzytkownika.addActionListener(this);
+		add(btnDodajUzytkownika);
 	}
 	
 	
@@ -40,9 +57,105 @@ public class PasswordFieldTest extends JFrame implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		JOptionPane.showMessageDialog(this, Arrays.toString(psfHaslo.getPassword()));
-		JOptionPane.showMessageDialog(this, String.valueOf(psfHaslo.getPassword()));
-		JOptionPane.showMessageDialog(this, new String(psfHaslo.getPassword()));
+		if (PanelHasla == null)
+		{
+			PanelHasla = new PanelHasla(this);
+		}
+		PanelHasla.setVisible(true);
+		PanelHasla.setFocus();
+		
+		if (PanelHasla.isOK() == true)
+		{
+			txaNotatnik.append(PanelHasla.getUser() + " , ");
+			txaNotatnik.append(PanelHasla.getPassword() + "\n");
+		}
+		else if (PanelHasla.isOK() == false)
+		{
+			
+		}
+//		JOptionPane.showMessageDialog(this, Arrays.toString(psfHaslo.getPassword()));
+//		JOptionPane.showMessageDialog(this, String.valueOf(psfHaslo.getPassword()));
+//		JOptionPane.showMessageDialog(this, new String(psfHaslo.getPassword()));
 	}
 
 }
+
+class PanelHasla extends JDialog implements ActionListener
+{
+	private JLabel lblUser, lblHaslo;
+	private JTextField txfUser, txfHaslo;
+	private JPasswordField psfHaslo;
+	private JButton btnOk, btnCancel;
+	private Boolean okData;
+	
+	public PanelHasla(JFrame owner)
+	{
+		super(owner, "Wprowadz haslo", true);
+		setSize(300 , 200);
+		setLayout(null);
+		
+		lblUser = new JLabel("Uzytkownik", JLabel.RIGHT);
+		lblUser.setBounds(0, 0, 100, 25);
+		add(lblUser);
+		
+		txfUser = new JTextField();
+		txfUser.setBounds(120, 0, 100, 25);
+		add(txfUser);
+		
+		
+		lblHaslo = new JLabel("Haslo", JLabel.RIGHT);
+		lblHaslo.setBounds(0, 30, 100, 25);
+		add(lblHaslo);
+		
+		psfHaslo = new JPasswordField();
+		psfHaslo.setBounds(50, 50, 150, 25);
+		psfHaslo.setBounds(120, 30, 100, 25);
+		psfHaslo.addActionListener(this);
+		add(psfHaslo);
+		
+		
+		btnOk = new JButton("OK");
+		btnOk.setBounds(0, 100, 100, 25);
+		btnOk.addActionListener(this);
+		add(btnOk);
+		
+		btnCancel = new JButton("Anuluj");
+		btnCancel.setBounds(100, 100, 100, 25);
+		btnCancel.addActionListener(this);
+		add(btnCancel);
+	}
+	
+	public String getUser()
+	{
+		return txfUser.getText();
+	}
+	public String getPassword()
+	{
+		return String.valueOf(psfHaslo.getPassword());
+	}
+	public boolean isOK()
+	{
+		return okData;
+	}
+	
+	public void setFocus()
+	{
+		txfUser.requestFocusInWindow();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		Object z = e.getSource();
+		if (z == btnOk)
+		{
+			okData = true;
+		}
+		else if (z == btnCancel)
+		{
+			okData = false;
+		}
+		setVisible(false);
+	}
+	
+}
+
